1b6e0fcd1c05ea2aa75778d9129fd8844ee649fe
mroll <mpr56@drexel.edu>
2017-05-03 19:38:39 +0000

    making an assets folder for static files

--- Wub/sciapp/routes/css.tcl
+++ Wub/sciapp/routes/css.tcl
@@ -1,5 +1,5 @@
 # [/css] is created by loading the contents of ./custom.css
 proc /css { r args } [subst {
-    set css {[::fread custom.css]};
+    set css {[::fread ${::sciapp_home}/assets/css/custom.css]};
     return \[Http Ok \$r \$css text/css\]
 }]
