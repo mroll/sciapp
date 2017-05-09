dcf1eab07b55779f20094953097e12478667d917
Tim Klein <timjklein36@gmail.com>
2017-05-09 14:04:52 -0400

    Add notes dialog box

--- Wub/sciapp/routes/hypthesis.tcl
+++ Wub/sciapp/routes/hypthesis.tcl
@@ -7,13 +7,16 @@ auth /hypothesis { r args } {
 	set page [<div> id "main-title" class "jumbotron" \
                   [<h1> Hypothesis]]
 
+	set noteText [<div> class input-group \
+				  {<input id="note-text" type="text" class="form-control" placeholder="Record your thoughts..."><span><button id="save-notes" class="btn btn-secondary" type="button">Save</button></span>}]
+
 	append page [_html::container \
                      [_html::row \
                           [_html::siblings \
                                [_html::box mainmenu \
-                                    title {Main Menu} \
+							   		-title {Main Menu} \
                                     -pos { my "left+15 top+15" at "left bottom" of ".jumbotron" } \
-                                    -width 300 \
+                                    -width 400 \
                                     [_html::nav \
 										 /dashboard {-text Dashboard} \
                                          /hypothesis {-text Hypothesis} \
@@ -22,12 +25,18 @@ auth /hypothesis { r args } {
                                          /analysis {-text Analysis} \
                                          /conclusions {-text Conclusions} \
                                          /test {-text Test -ids {question} -style inv}]] \
+							   [_html::box notes \
+									-title {Notes} \
+									-pos { my "center top+15" at "center bottom" of ".jumbotron" } \
+									-width 400 \
+									[_html::siblings $noteText]] \
                                [_html::box logout \
-                                    title {Logout} \
+                                    -title {Logout} \
                                     -pos { my "right-15 top+15" at "right bottom" of ".jumbotron" } \
-                                    -width 300 \
+                                    -width 400 \
                                     -hidetitle 1 \
-                                    [<div> [_html::nav /logout {-text Logout}]]]]]]
+                                    [<div> [_html::nav /logout {-text Logout}]]] \	
+							   ]]]
 
 	set r [Html style $r css]
 	return [Http Ok $r $page]
